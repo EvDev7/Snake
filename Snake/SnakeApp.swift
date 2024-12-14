afaaff5333383accb79790daba8933087e71f2bb
Evan Rinehart <evanrinehart1@gmail.com>
2024-12-13 23:44:48 -0500

    Initial Commit

---
+++ Snake/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Evan Rinehart on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnakeTests/SnakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeTests.swift
+//  SnakeTests
+//
+//  Created by Evan Rinehart on 12/13/24.
+//
+
+import Testing
+@testable import Snake
+
+struct SnakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
